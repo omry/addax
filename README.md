44e1bf765f5ab69cf40f5b6585be995dedc77cd6
omry <omry@yadan.net>
2019-07-10 22:52:21 -0700

    adding testml and yaml-test-suite submodules

---
+++ README.md
@@ -0,0 +1,4 @@
+# Elk
+Elk is an [ANTLR](https://www.antlr.org/) based YAML 1.2 parser.
+
+Work in progress
---
+++ python/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    LONG_DESC = fh.read()
+    setuptools.setup(
+        name="elk2",
+        version="0.1",
+        author="Omry Yadan",
+        author_email="omry@yadan.net",
+        description="YAML to AST parser",
+        long_description=LONG_DESC,
+        long_description_content_type="text/markdown",
+        setup_requires=["pytest-runner"],
+        tests_require=["pytest"],
+        url="https://github.com/omry/elk",
+        keywords='yaml parser',
+        packages=['elk2'],
+        classifiers=[
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "License :: OSI Approved :: BSD License",
+            "Operating System :: OS Independent",
+        ],
+        install_requires=[
+            'antlr4-python3-runtime;python_version>="3.0"',
+            'antlr4-python2-runtime;python_version<"3.0"',
+        ]
+    )
