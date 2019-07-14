f50a722ad553678c88ae295b9b091c34fc6a2356
omry <omry@yadan.net>
2019-07-14 07:57:38 -0700

    gives a helpful error if antlr parser is not generated

--- python/antelope/__init__.py
+++ python/antelope/__init__.py
@@ -1,12 +1,14 @@
 import sys
 
-if sys.version_info >= (3, 0):
-    from .gen3 import YAMLListener
-    from .gen3 import YAMLLexer
-    from .gen3 import YAMLParser
-else:
-    from .gen2 import YAMLListener
-    from .gen2 import YAMLLexer
-    from .gen2 import YAMLParser
-
+try:
+    if sys.version_info >= (3, 0):
+        from .gen3 import YAMLListener
+        from .gen3 import YAMLLexer
+        from .gen3 import YAMLParser
+    else:
+        from .gen2 import YAMLListener
+        from .gen2 import YAMLLexer
+        from .gen2 import YAMLParser
+except ImportError as err:
+    raise ImportError("Error importing generated parser, run setup.py antlr to generate")
 from .antelope import Antelope
